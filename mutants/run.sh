69ba1800ed876c4c53352daccf586c4e5dcf0d3f
Hobin Yoon <hobinyoon@gmail.com>
2016-09-05 18:29:56 +0000

    more updates

--- mutants/run.sh
+++ mutants/run.sh
@@ -8,11 +8,12 @@ DN_THIS=`dirname $BASH_SOURCE`
 pushd $DN_THIS/.. >/dev/null
 
 bin/ycsb run cassandra-cql \
--P workloads/workloada \
+-P workloads/workloadd \
 -s \
 -threads 1 \
 -p hosts=`cat ~/work/mutants/.run/cassandra-server-ips` \
 -p recordcount=1000 \
--p operationcount=10000
+-p operationcount=1000 \
+| grep --color=always -E "^|Mutants"
 
 popd >/dev/null
