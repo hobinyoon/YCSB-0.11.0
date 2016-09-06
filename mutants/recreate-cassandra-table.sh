9ed56b3ba864cccdb619cec352d8a226e135eaee
Hobin Yoon <hobinyoon@gmail.com>
2016-09-06 05:09:56 +0000

    update

--- mutants/recreate-cassandra-table.sh
+++ mutants/recreate-cassandra-table.sh
@@ -1,1 +1,3 @@
-cqlsh -f recreate-cassandra-table.cql `cat ~/work/mutants/.run/cassandra-server-ips`
+DN_THIS=`dirname $BASH_SOURCE`
+
+cqlsh -f $DN_THIS/recreate-cassandra-table.cql `cat ~/work/mutants/.run/cassandra-server-ips`
--- mutants/run-d.sh
+++ mutants/run-d.sh
@@ -11,15 +11,26 @@ DATETIME=`date +"%y%m%d-%H%M%S"`
 
 mkdir -p "mutants/.log"
 
-YCSB_LOG="mutants/.log/ycsb-"$DATETIME
+YCSB_LOG="mutants/.log/"$DATETIME"-d"
 
-bin/ycsb run cassandra-cql \
+time -p mutants/recreate-cassandra-table.sh
+echo
+sleep 1
+
+time -p mutants/load.sh
+echo
+sleep 0.1
+
+time -p bin/ycsb run cassandra-cql \
 -P workloads/workloadd \
 -s \
--threads 1 \
 -p hosts=`cat ~/work/mutants/.run/cassandra-server-ips` \
 -p recordcount=1000 \
--p operationcount=1000 \
+-p operationcount=10000000 \
+-p status.interval=1 \
+-p fieldcount=10 \
+-p fieldlength=2000 \
+-threads 50 \
 2>&1 \
 | tee -a $YCSB_LOG | grep --color=always -E "^|Mutants"
 
