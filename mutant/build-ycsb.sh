98940b9649003952c3b4626f0ff7237eb438fccc
Hobin Yoon <hobinyoon@gmail.com>
2016-10-04 17:10:32 +0000

    reorg. add mongodb

--- mutant/build-ycsb.sh
+++ mutant/build-ycsb.sh
@@ -8,5 +8,6 @@ DN_THIS=`dirname $BASH_SOURCE`
 pushd $DN_THIS/.. >/dev/null
 
 mvn -pl com.yahoo.ycsb:cassandra-binding -am clean package -DskipTests
+mvn -pl com.yahoo.ycsb:mongodb-binding -am clean package -DskipTests
 
 popd >/dev/null
