d0c79c97f1e26167c50c675d75186ed78c03d1b7
Hobin Yoon <hobinyoon@gmail.com>
2016-11-08 12:36:32 -0500

    stop dstat when done. udpate.

--- mutant/cassandra/restart-dstat-run-workload.py
+++ mutant/cassandra/restart-dstat-run-workload.py
@@ -11,7 +11,7 @@ import Cons
 import Util
 
 sys.path.insert(0, "%s/work/mutant/ec2-tools/lib" % os.path.expanduser("~"))
-import BotoClient
+import Ec2Util
 
 
 _cur_datetime = datetime.datetime.now().strftime("%y%m%d-%H%M%S")
@@ -19,20 +19,20 @@ _cur_datetime = datetime.datetime.now().strftime("%y%m%d-%H%M%S")
 def main(argv):
 	RestartDstat()
 	RunYcsb(argv)
+	StopDstat()
 
 
 def RunYcsb(argv):
 	dn_ycsb_log = "%s/work/mutant/log/%s/%s/ycsb" \
-			% (os.path.expanduser("~")
-					, GetEc2Tags()["job_id"]
-					, GetEc2Tags()["name"].replace("server", "s").replace("client", "c"))
+			% (os.path.expanduser("~"), Ec2Util.JobId(), Ec2Util.NodeName())
 	Util.MkDirs(dn_ycsb_log)
 
 	if len(argv) >= 3:
 		workload_type = argv[1]
 		ycsb_params = " ".join(argv[i] for i in range(2, len(argv)))
 	else:
-		workload_type = "a"
+		workload_type = "c"
+		ycsb_params = " ".join(argv[i] for i in range(2, len(argv)))
 
 	fn_log = "%s/%s-%s" % (dn_ycsb_log, _cur_datetime, workload_type)
 
@@ -40,23 +40,6 @@ def RunYcsb(argv):
 			% (os.path.dirname(__file__), workload_type, ycsb_params, fn_log)
 	Util.RunSubp(cmd)
 	Cons.P("Created %s %d" % (fn_log, os.path.getsize(fn_log)))
-
-
-_tags = None
-def GetEc2Tags():
-	global _tags
-	if _tags is not None:
-		return _tags
-
-	r = BotoClient.Get(GetRegion()).describe_tags()
-	#Cons.P(pprint.pformat(r, indent=2, width=100))
-	_tags = {}
-	for r0 in r["Tags"]:
-		res_id = r0["ResourceId"]
-		if InstId() != res_id:
-			continue
-		_tags[r0["Key"]] = r0["Value"]
-	return _tags
 
 
 _region = None
@@ -86,6 +69,7 @@ def InstId():
 	return _inst_id
 
 
+# May want to make it an object and use RAII
 def RestartDstat():
 	with Cons.MT("Restarting dstat ...", print_time=False):
 		cmd = "ps -e -o pid,ppid,user,args"
@@ -124,14 +108,49 @@ def RestartDstat():
 
 		# Run dstat as a daemon
 		dn = "%s/work/mutant/log/%s/%s/dstat" \
-				% (os.path.expanduser("~")
-						, GetEc2Tags()["job_id"]
-						, GetEc2Tags()["name"].replace("server", "s").replace("client", "c"))
+				% (os.path.expanduser("~"), Ec2Util.JobId(), Ec2Util.NodeName())
 		Util.MkDirs(dn)
 
 		fn_out = "%s/%s.csv" % (dn, _cur_datetime)
 		cmd = "dstat -tcdn -C total -D xvda,xvdb,xvdd,xvde,xvdf -r --output %s" % fn_out
 		Util.RunDaemon(cmd)
+
+
+def StopDstat():
+	with Cons.MT("Stopping dstat ...", print_time=False):
+		cmd = "ps -e -o pid,ppid,user,args"
+		lines = Util.RunSubp(cmd, print_cmd=False, print_output=False)
+		#Cons.P(lines)
+		pids = []
+		for line in lines.split("\n"):
+			line = line.strip()
+			if "dstat" not in line:
+				continue
+			if "csv" not in line:
+				continue
+
+			# Get the second-level processes, skipping the root-level ones.
+			t = re.split(" +", line)
+			if t[1] == "1":
+				continue
+			pids.append(t[0])
+			#Cons.P("[%s]" % line)
+
+		if len(pids) > 0:
+			#Cons.P("[%s]" % " ".join(pids))
+			Util.RunSubp("kill %s" % " ".join(pids))
+
+			# Make sure each of the processes has terminated
+			for pid in pids:
+				cmd = "kill -0 %s" % pid
+				while True:
+					r = 0
+					with open(os.devnull, "w") as devnull:
+						r = subprocess.Popen(cmd, shell=True, stdin=devnull, stdout=devnull, stderr=devnull)
+					if r != 0:
+						Cons.P("Process %s has terminated" % pid)
+						break
+					time.sleep(0.1)
 
 
 if __name__ == "__main__":
